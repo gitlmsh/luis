019783ec76782a022d4199401aa9524958ba4adf
gitlmsh <167395651+gitlmsh@users.noreply.github.com>
2024-04-22 13:39:39 -0600

    Update4 codigo.py

--- codigo.py
+++ codigo.py
@@ -1,5 +1,5 @@
 nombre = "yo soy el que soy 2"
-apellido = "sanchez her"
+apellido = "sanchez hernandez"
 
 saludo= print( "Hello"+ " "+ nombre + " " + apellido + " "+ "¿como estas?")
 print(saludo + " asds")
